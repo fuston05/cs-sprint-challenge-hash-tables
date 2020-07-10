0e5b1d62bb562197c7c10375ad581101e2ec7012
fuston05 <fuston05@yahoo.com>
2020-07-10 19:40:36 -0400

    almost got ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,61 +2,23 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    cache= {}
+    cache = {}
     # Your code here
+    string = ''
     for i in a:
-      if i not in cache:
-        cache[i]= None 
+        string += str(i)+' '
+    string= string.split()
 
-    result= []
-    for k, v in cache.items():
-      k= str(k)
-      if k[0] == '-':
-
-        for i, j in cache.items():
-          i= str(i)
-          if i == k[1]:
-            result.append(int(k[1]))
-
-        
-      # print(k)
-
-    return(result)
-
-      # if i != '' and i not in cache:
-      #   cache[i]= 1
-      # else: 
-      #   cache[i]+=1
+    for i in string:
+        # print(i)
+        if i not in cache:
+            if i[0] == '-':
+                cache[i] = 'neg'
+            else: 
+              cache[i]= 'pos'
 
 
-    # # remove the '-'
-    # for i in string:
-    #   if i[0] == '-':
-    #     string= string.replace(i, '')
-
-    # string= string.split()
-
-    # # any dups we have means one had a '-', so return it
-    # for i in string:
-    #   if i not in cache:
-    #     cache[i]= 1
-    #   else: 
-    #     cache[i]+=1
-
-    # result= []
-    # for k, v in cache.items():
-    #   if v > 2:
-    #     result.append(k)
-        
-    
-
-      
-    
-
-
-    
-
-
+    return cache
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
