0305b18391307f62ccda200b25d83dbd28f9d26a
fuston05 <fuston05@yahoo.com>
2020-07-10 17:45:16 -0400

    passes ex4.py"

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -2,15 +2,58 @@ def has_negatives(a):
     """
     YOUR CODE HERE
     """
+    cache= {}
     # Your code here
-    string= ''
     for i in a:
-      string+= str(i)+' '
+      if i not in cache:
+        cache[i]= None 
 
-    for i in string:
-      i= i.strip('-')
+    result= []
+    for k, v in cache.items():
+      k= str(k)
+      if k[0] == '-':
+
+        for i, j in cache.items():
+          i= str(i)
+          if i == k[1]:
+            result.append(int(k[1]))
+
+        
+      # print(k)
+
+    return(result)
+
+      # if i != '' and i not in cache:
+      #   cache[i]= 1
+      # else: 
+      #   cache[i]+=1
+
+
+    # # remove the '-'
+    # for i in string:
+    #   if i[0] == '-':
+    #     string= string.replace(i, '')
+
+    # string= string.split()
+
+    # # any dups we have means one had a '-', so return it
+    # for i in string:
+    #   if i not in cache:
+    #     cache[i]= 1
+    #   else: 
+    #     cache[i]+=1
+
+    # result= []
+    # for k, v in cache.items():
+    #   if v > 2:
+    #     result.append(k)
+        
     
-    return string
+
+      
+    
+
+
     
 
 
