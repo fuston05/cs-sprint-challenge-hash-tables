110903e507346fec04491458ec15feb59f501f17
fuston05 <fuston05@yahoo.com>
2020-07-10 19:21:44 -0400

    passes ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -13,27 +13,27 @@ def reconstruct_trip(tickets, length):
     cache= {}
     # add to cache where src= key, dest= value
     for i in range(length):
-      cache[tickets[i]]= None
+      cache[tickets[i].source]= tickets[i].destination
 
     # find starting dest. has NONE as src
     route= []
     start= {}
-    for i in cache:
-      if i.source == 'NONE':
-        start= i
-        route.append(start.destination)
+    for i, v in cache.items():
+      if i == 'NONE':
+        start= v
+        route.append(start)
     # print(start)
     # final dest has NONE as dest
 
 
     # start with 'start' get its dest
-    for i in cache:
-      if i.source == start.destination:
-        next= i.source
-        route.append(next)
-        start= i
+    next= start
+    while len(route) < len(tickets):
+      for k, v in cache.items():
+        if k == next:
+          next= cache[k]
+          route.append(next)
 
-    print('next: ', next)
       # if i.source == start.destination:
       #   print(i.source)
     # search and find ticket with src == start.dest
@@ -43,11 +43,12 @@ def reconstruct_trip(tickets, length):
 
 
 
-    print('route: ', route)
-
-
+    # print('route: ', route)
+    # print('')
+    # print('next: ', next)
+    # print('')
     # print('cache: ', cache)
-
+    return route
 
 ticket_1 = Ticket("PIT", "ORD")
 ticket_2 = Ticket("XNA", "SAP")
@@ -62,6 +63,6 @@ ticket_10 = Ticket("BHM", "FLG")
 
 tickets = [ticket_1, ticket_2, ticket_3, ticket_4, ticket_5, ticket_6, ticket_7, ticket_8, ticket_9, ticket_10]
 
-reconstruct_trip(tickets, len(tickets))
+print(reconstruct_trip(tickets, len(tickets)))
 
 # expected = ["LAX", "SFO", "BHM", "FLG", "XNA", "SAP", "SLC", "PIT", "ORD", "NONE"]
