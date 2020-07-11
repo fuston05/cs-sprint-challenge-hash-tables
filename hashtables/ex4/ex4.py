592c57292115edb669143e4c46ad4cb99013c234
fuston05 <fuston05@yahoo.com>
2020-07-11 19:06:28 -0400

    cleans up ex4.py

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,32 +4,21 @@ def has_negatives(a):
     """
     cache = {}
     # Your code here
+    # reverse so we can start iterating over + nums
     a.sort(reverse= True)
 
     for i in a:
+      # add to cache
       if i not in cache:
         cache[i]= None
 
     result= []
+
     for c in cache:
+      # formula to find neg opposite
       isNeg= c- (c*2)
       if isNeg in cache:
         if c > 0:
           result.append(c)
-
-    result.sort()
+          
     return result
-
-if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
-    #                     [-4, -2, -1, 1, 2, 3, 4]
-
-    # print(has_negatives([1,2,3]))
-    # self.assertTrue(result == [])
-
-    # print(has_negatives([1,2,3,-4]))
-    # # self.assertTrue(result == [])
-
-    # print(has_negatives([-1,-2,1,2,3,4,-4]))
-    # result.sort()
-    # self.assertTrue(result == [1,2,4])
