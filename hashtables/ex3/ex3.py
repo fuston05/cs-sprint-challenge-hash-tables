0746fd296c3c04f5b5ed23292ac6372f6ada4b7e
fuston05 <fuston05@yahoo.com>
2020-07-12 17:56:10 -0400

    passes ex3.py

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,26 +1,39 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
-    cache1= {}
-    cache2= {}
+  """
+  YOUR CODE HERE
+  """
+  
+  cache= {}
+  res= []
 
-    for i in range(len(arrays)):
-      cache1[i]= arrays[i]
-      cache2[i]= arrays[i]
+  listCount= 0
+  for list in arrays: 
+    # list is a sub array
+    #remove duplicates
+    list= set(list)
 
-    for i in range(len(arrays-1)):
-      if cache1[i] in cache2[i]
+    for num in list:
+    # if num not in cache add it.
+      if num not in cache:
+        cache[num]= 1
+      elif num in cache and listCount > 0:
+        # if it IS in cache, increment its counter
+        cache[num]+= 1
+    listCount+= 1
 
-    return cache[0]
+    # find all counters that equal the num of sub lists that were passed in
+    for k,v in cache.items():
+      if v == len(arrays):
+        res.append(k)
+
+  return res
 
 
 if __name__ == "__main__":
-    arrays = []
+  arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+  arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+  arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+  arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+  print(intersection(arrays))
