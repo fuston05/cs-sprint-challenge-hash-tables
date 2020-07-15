9d1e7435ac0612859a915f191b98e32e22fff58c
fuston05 <fuston05@yahoo.com>
2020-07-14 21:13:52 -0400

    passes ex1.py

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,37 +1,25 @@
 def get_indices_of_item_weights(weights, length, limit):
     cache= {}
+    res= []
     """
     YOUR CODE HERE
     """
     # Your code here
+    if len(weights) == 1:
+        return None
     # 
-    # add weights to dict with index as value
-    for i in range(length):
-        if weights[i] not in cache:
-            cache[weights[i]]= [i]
-        else: cache[weights[i]].append(i)
-    
+    # add weights to dict with weight as value, index as key
+    # to handle duplicate weights
+    for ind, weight in enumerate(weights):
+        if ind not in cache:
+            cache[ind]= weight
 
+    for ww, vv  in cache.items():
+        for k, v in cache.items():
+            # compare indicies (keys == indicies, values== weights)
+            if ww != k:
+                added= vv  + v
+                if added == limit:
+                    res.append(ww)
 
-
-    return cache
-
-    # iterate dict adding all key pairs of weights
-      # create a list of tuples containg weights indicies pair and their total value
-      #        ind, ind, total
-      # list[ (4, 5, 9), (2, 6, 8), (8, 4, 12) ]
-    
-    # then we can iterate list checking list[i][2] against limit
-    # if it matches return list[i][0], list[i][1]
-
-weights_1 = [9]
-weights_2 = [4, 4]
-weights_3 = [4, 6, 10, 15, 16]
-weights_4 = [12, 6, 7, 14, 19, 3, 0, 25, 40]
-# print(get_indices_of_item_weights(weights_1, 1, 9))
-print('')
-# print(get_indices_of_item_weights(weights_2, 2, 8))
-print('')
-print(get_indices_of_item_weights(weights_3, 5, 21))
-print('')
-# print(get_indices_of_item_weights(weights_4, 9, 7))
+    return res
