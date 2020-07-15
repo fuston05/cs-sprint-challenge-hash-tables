afe68466e35914f389ba6e68e7cdc4bd6ec15ce5
fuston05 <fuston05@yahoo.com>
2020-07-14 21:36:42 -0400

    cleanded up code to be more readable, added comments

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -8,18 +8,19 @@ def get_indices_of_item_weights(weights, length, limit):
     if len(weights) == 1:
         return None
     # 
-    # add weights to dict with weight as value, index as key
-    # to handle duplicate weights
+    # add weights to dict with weight as value, and indicies as key
     for ind, weight in enumerate(weights):
         if ind not in cache:
             cache[ind]= weight
 
+    #  ind  weight
     for ww, vv  in cache.items():
+        # ind weight
         for k, v in cache.items():
-            # compare indicies (keys == indicies, values== weights)
+            # comparr indicies so we dont calc ind[1] with ind[1]
             if ww != k:
                 added= vv  + v
                 if added == limit:
                     res.append(ww)
-
+    print('res rev: ', res.reverse())
     return res
